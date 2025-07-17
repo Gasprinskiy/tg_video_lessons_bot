7892947dd6547da747ccf269b356854512ea9afc
samgasper <zakirov.ismail@sarkor.uz>
2025-07-17 18:02:02 +0500

    feat: init chronos tool to work with time

---
+++ src/tools/chronos/chronos.go
@@ -0,0 +1,14 @@
+package chronos
+
+const (
+	// DateTimeMask маска с датой и временем
+	DateTimeMask = "02.01.2006 15:04:05"
+	// DateTimeWithoutSecondMask маска с датой и временем без секунд
+	DateTimeWithoutSecondMask = "02.01.2006 15:04"
+	// DateTimeMaskLocale маска с датой и временем с зоной
+	DateTimeMaskLocale = "02.01.2006 15:04:05 -0700"
+	// DateMask маска с датой
+	DateMask = "02.01.2006"
+	// DateMaskLocale маска с датой и зоной
+	DateMaskLocale = "02.01.2006 -0700"
+)
