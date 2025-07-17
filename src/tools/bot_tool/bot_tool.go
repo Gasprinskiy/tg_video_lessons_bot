4ec675c10ec47457d13891c54a814fb865deb81a
samgasper <zakirov.ismail@sarkor.uz>
2025-07-17 18:01:02 +0500

    feat: reply keyboard message tool

--- src/tools/bot_tool/bot_tool.go
+++ src/tools/bot_tool/bot_tool.go
@@ -2,6 +2,7 @@ package bot_tool
 
 import (
 	"context"
+	"tg_video_lessons_bot/internal/entity/global"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -17,5 +18,25 @@ func SendHTMLParseModeMessage(
 		ChatID:    update.Message.Chat.ID,
 		Text:      message,
 		ParseMode: "HTML",
+	})
+}
+
+func SendReplyKeyboardMessage(
+	ctx context.Context,
+	b *bot.Bot,
+	update *models.Update,
+	replyMessage global.ReplyMessage,
+	closeAfterClick bool,
+) {
+	b.SendMessage(ctx, &bot.SendMessageParams{
+		ChatID: update.Message.Chat.ID,
+		Text:   replyMessage.Message,
+		ReplyMarkup: &models.ReplyKeyboardMarkup{
+			Keyboard: [][]models.KeyboardButton{
+				replyMessage.ButtonList,
+			},
+			ResizeKeyboard:  true,
+			OneTimeKeyboard: closeAfterClick,
+		},
 	})
 }
