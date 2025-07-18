fc007402782147ef20fb57bc165c1163e9510ef2
samgasper <zakirov.ismail@sarkor.uz>
2025-07-18 17:26:17 +0500

    feat: sesstion manager in uimport

--- src/uimport/imports.go
+++ src/uimport/imports.go
@@ -1,16 +1,22 @@
 package uimport
 
 import (
+	"tg_video_lessons_bot/internal/transaction"
 	"tg_video_lessons_bot/internal/usecase"
 	"tg_video_lessons_bot/rimport"
 )
 
 type UsecaseImport struct {
+	SessionManager transaction.SessionManager
 	Usecase
 }
 
-func NewUsecaseImport(ri *rimport.RepositoryImports) *UsecaseImport {
+func NewUsecaseImport(
+	ri *rimport.RepositoryImports,
+	sessionManager transaction.SessionManager,
+) *UsecaseImport {
 	return &UsecaseImport{
+		SessionManager: sessionManager,
 		Usecase: Usecase{
 			Profile: usecase.NewProfile(ri),
 		},
