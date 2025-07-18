f27fd1b3e35241fed2059aba43d7e6a45b629c70
samgasper <zakirov.ismail@sarkor.uz>
2025-07-18 17:25:53 +0500

    feat: move redis connect from rimport

--- src/rimport/imports.go
+++ src/rimport/imports.go
@@ -1,8 +1,6 @@
 package rimport
 
 import (
-	"context"
-	"log"
 	"tg_video_lessons_bot/config"
 	"tg_video_lessons_bot/internal/repository/redis_cache"
 
@@ -10,23 +8,11 @@ import (
 )
 
 type RepositoryImports struct {
-	RedisDB *redis.Client
-	Config  *config.Config
 	Repository
 }
 
-func NewRepositoryImports(config *config.Config) *RepositoryImports {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     config.RedisAddr,
-		Password: config.RedisPass,
-	})
-
-	if _, err := rdb.Ping(context.Background()).Result(); err != nil {
-		log.Panic("ошибка при пинге redis: ", err)
-	}
-
+func NewRepositoryImports(config *config.Config, rdb *redis.Client) *RepositoryImports {
 	return &RepositoryImports{
-		RedisDB: rdb,
 		Repository: Repository{
 			UserCache: redis_cache.NewUserCache(rdb, config.RedisTtl),
 		},
