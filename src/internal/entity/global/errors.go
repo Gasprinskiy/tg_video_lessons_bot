058082e677b541db0eaf99d0c25c8ef62f682fc6
Gasprinskiy <gasprin321@gmail.com>
2025-07-19 18:12:26 +0500

    feat: persmisson denied error message

--- src/internal/entity/global/errors.go
+++ src/internal/entity/global/errors.go
@@ -10,10 +10,13 @@ var (
 	ErrNoData = errors.New("данные не найдены")
 	// ErrInternalError внутряя ошибка
 	ErrInternalError = errors.New("произошла внутреняя ошибка")
+	//
+	ErrPermissionDenied = errors.New("отказано в доступе")
 )
 
 var MessagesByError = map[error]string{
 	ErrInternalError:              "Внутреняя ошибка бота, попробуйте позже или свяжитесь с поддержкой.",
+	ErrPermissionDenied:           "Отказано в доступе выполнения команды",
 	profile.ErrFullNameValidation: "Имя и фамилия введены не верно, попробуйте еще раз",
 	profile.ErrBirthDateInFuture:  "Дата рождения не может быть в будущем",
 	profile.ErrBirhDateInvalid:    "Дата рождения введено не верно, попробуйте еще раз",
