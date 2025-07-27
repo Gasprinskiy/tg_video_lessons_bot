95811ff7a87c3e58dbd0320ee7cc841855e51b8b
Gasprinskiy <gasprin321@gmail.com>
2025-07-27 18:47:08 +0500

    chore: move redis to global level

--- get_pg_env.sh
+++ get_pg_env.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+PARENT_ENV="../.env"
+
+if [ ! -f "$PARENT_ENV" ]; then
+  echo "Global .env file not found, local .env file will be used, insure that all variables in it"
+  exit 0
+fi
+
 echo "Get postgres env"
 
-PARENT_ENV="../.env"
 LOCAL_ENV="./.env"
 
-VARS="POSTGRES_USER POSTGRES_PASSWORD POSTGRES_DB POSTGRES_INNER_PORT POSTGRES_HOST"
+VARS="POSTGRES_USER POSTGRES_PASSWORD POSTGRES_DB POSTGRES_INNER_PORT POSTGRES_HOST REDIS_PASSWORD REDIS_PORT"
 
 for VAR in $VARS; do
   LINE=$(grep -E "^[[:space:]]*${VAR}[[:space:]]*=" "$PARENT_ENV" | grep -v '^[[:space:]]*#' | tail -n 1)
